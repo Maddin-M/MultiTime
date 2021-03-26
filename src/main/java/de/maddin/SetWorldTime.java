20c1c035a58e8fa035cc660b40ebb08f74ae494f
Maddin <martin.manka@gmx.de>
2021-03-26 15:09:32 +0100

    init

---
+++ src/main/java/de/maddin/SetWorldTime.java
@@ -0,0 +1,16 @@
+package de.maddin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SetWorldTime extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
